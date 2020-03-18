adcbb2942516bea4433c031b8d2b0406b39a7e63
Israel Ogbole <iogbole@gmail.com>
2020-03-18 13:28:51 +0000

    remove test folder

--- test/merge_json-files.sh
+++
@@ -1,5 +0,0 @@
-jq -s '[ .[0] + .[1] | group_by(.apiproxy)[] | select(length > 1) | add ]'  all*.json
-
-
-
-#jq -s 'include "joins"; joins(.apiproxy)' all*.json
--- test/transpose_dimensions.sh
+++
@@ -1,6 +0,0 @@
- jq  '.[].stats.data[]
-    | [.identifier.names, .identifier.values]
-    | transpose
-    | map({(.[0]): .[1]})
-    | add     
-    ' < response.json
